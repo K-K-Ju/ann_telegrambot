443cdd731066dee0cd35a7041079b9b0ddfbfa66
K-K-Ju <andrunivermail@gmail.com>
2024-06-01 11:54:43 +0300

    Minor changes

--- static.py
+++ static.py
@@ -9,7 +9,7 @@ class States(Enum):
     S_SEND_PIC = 4
 
 
-welcome_msg = ("""Welcome to the bot. It can generate and find images using your prompt. Give it a try describe any picture you want to receive""")
+welcome_msg = """Welcome to the bot. It can generate and find images using your prompt. Give it a try describe any picture you want to receive"""
 continue_msg = "Send next prompt or configure image search"
 enter_number_msg = "Enter number of photos you want to receive"
 enter_orientation_msg = "Now enter desired orientation (landscape, portrait)"
